cba371f9fac8baf699a6fbf45935b68126bad6ee
Cang <cang.dinh@deutschebahn.com>
2021-02-22 18:10:27 +0100

    Chapter 3

---
+++ twotter-tutorial/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+   @{{ user.username }} - {{ fullName }}
+   <p></p>
+   <strong>Followers: </strong> {{ followers }}
+   <p></p>
+   <button
+    @click="followUser">
+    Follow
+   </button>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_Foobar',
+        firstName: 'Foo',
+        lastName: 'Baz',
+        email: 'hab@ich.net',
+        isAdmin: true
+      }      
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  },
+  mounted() {
+    this.followUser();
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} gained a follower`)
+      }
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
